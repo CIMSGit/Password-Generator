fa33ca516ad188e7d98cf8187cef8e5a39f4f08d
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-01-10 02:01:03 +0000

    updates while loop criterias

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -99,8 +99,8 @@ function generatePassword() {
 // adds while loop to get the characters between 10 to 64
  //  creates if statements for building up the new array by concatination
 
-while (charactersNumber < 10 || charactersNumber >64 || charactersNumber!=Number ) {
-  alert("Please choose between 10 and 64 characters and type a number");
+while (charactersNumber < 10 || charactersNumber >64 ) {
+  alert("Please choose between 10 and 64 characters");
   alert("Click again on Generate Password")
   return "Try again, please!";
 
